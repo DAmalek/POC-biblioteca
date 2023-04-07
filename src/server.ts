fcf245e46cbf5e70fcfe978e8500733b8788475d
Daniel Abdel <danielmalek101@gmail.com>
2023-04-07 02:19:43 -0300

    feat: server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Router } from "express";
 import dotenv from "dotenv";
 import route from "./routes/bookRoute.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -8,8 +9,12 @@ const server = express();
 
 server.use(express.json());
 server.use(cors());
+
+server.get("/health", (req, res) => {
+  res.send("ok ;(");
+});
 server.use([route]);
 
 server.listen(process.env.PORT, () => {
-  console.log("server bombando!!!");
+  console.log("server bombando!!!!!");
 });
